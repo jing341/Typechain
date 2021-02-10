a624b45c2eae1882261acbeb3cbf48c812295eed
jing341 <takio0116@gmail.com>
2021-02-10 22:34:51 +0900

    Blockchain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-const hello = "hello";
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
 
-console.log(hello);
+const genesisBlock: Block = new Block(0, "hello", "", "", 123);
+
+let blockChain: [Block] = [genesisBlock];
+
+console.log(blockChain);
+
+export {};
